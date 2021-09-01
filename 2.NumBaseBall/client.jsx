a085a62245ab3b725b527bfe1985aad64d77207c
kki4504 <rhkddlf9876@gmail.com>
2021-09-01 20:48:34 +0900

    NumBaseBall day 1

---
+++ 2.NumBaseBall/client.jsx
@@ -0,0 +1,6 @@
+const React = require('react');
+const ReactDom = require('react-dom');
+
+const NumBaseBall = require('./NumBaseBall');
+
+ReactDom.render(<NumBaseBall />, document.querySelector('#root'));
