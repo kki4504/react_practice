3ae641583d32f42b81a00774afcf107f14fb92ee
kki4504 <rhkddlf9876@gmail.com>
2022-06-27 19:43:17 +0900

    EDIT: webpack.config.js

--- 끝말잇기/webpack.config.js
+++ 끝말잇기/webpack.config.js
@@ -1,26 +1,39 @@
 const path = require('path');
 const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const webpack = require('webpack')
 
 module.exports = {
     name: 'wordRelay-setting',
     mode: 'development', // 실서비스 : production
     devtool: 'eval',
     resolve: {
-        extensions: ['.js', '.jsx']
+        extensions: ['.js', '.jsx'] // entry app에 확장자를 알아서 찾아줌
     },
 
     entry: {
         app: ['./client'],
     }, // 입력
 
-    modules: {
+    module: {
         rules: [{
             test: /\.jsx?/,
             loader: 'babel-loader',
             options: {
                 presets: [
-                    '@babel/preset-env',
-                    '@babel/preset-react',
+                    ['@babel/preset-env', {
+                        targets: {
+                          browsers: [
+                            '> 5% in KR',
+                            'last 2 chrome versions',
+                          ],
+                        },
+                        debug: true,
+                      }], 
+                      ['@babel/preset-react', {
+                        targets: {
+                          browsers: [],
+                        }
+                      }]
                 ],
                 plugins: [
                     '@babel/plugin-proposal-class-properties',
@@ -30,7 +43,8 @@ module.exports = {
         }],
     },
     plugins: [
-            new s()
+            new RefreshWebpackPlugin(),
+            // new webpack.LoaderOptionsPlugin({ debug: true }),
         ],
 
     output: {
@@ -40,6 +54,7 @@ module.exports = {
         publicPath: './dist/',
     }, // 출력
     devServer: {
+        // publicPath: './dist/',
         devMiddleware: { publicPath: '/dist' },
         static: { directory: path.resolve(__dirname) },
         hot: true
