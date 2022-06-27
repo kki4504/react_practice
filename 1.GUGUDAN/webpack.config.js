99b3964465e6e349b06266cbee6f6cff56c0ccb0
kki4504 <rhkddlf9876@gmail.com>
2022-06-27 19:51:02 +0900

    EDIT: webpack.config.js

--- 1.GUGUDAN/webpack.config.js
+++ 1.GUGUDAN/webpack.config.js
@@ -1,41 +1,46 @@
 const path = require('path');
+const webpack = require('webpack')
+// const webpackConfig = require('../끝말잇기/webpack.config');
 
 module.exports = {
-    mode: 'development',
-    devtool: 'eval', // hidden-source-map
-    resolve: {
-        extensions: ['.jsx', '.js']
-    },
-    entry: {
-        app: './client',
-    },
-    module: {
-        rules: [{
-            test: /\.jsx?$/,
-            loader: 'babel-loader',
-            options: {
-                presets: [
-                    ['@babel/preset-env', {
-                        targets: {
-                            browsers: [
-                                '> 5% in KR',
-                                'last 2 chrome versions',
-                            ],
-                        }
-                    }], 
-                    ['@babel/preset-react', {
-                        targets: {
-                            browsers: [],
-                        }
-                    }]
-                ],
-                plugins: [],
-            },
-        }],
-    },
-    plugins: [],
-    output: {
-        filename: 'app.js',
-        path: path.join(__dirname, 'dist'),
-    }
+  mode: 'development',
+  devtool: 'eval', // hidden-source-map
+  resolve: {
+    extensions: ['.jsx', '.js']
+  },
+  entry: {
+    app: './client',
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      options: {
+        presets: [
+          ['@babel/preset-env', {
+            targets: {
+              browsers: [
+                '> 5% in KR',
+                'last 2 chrome versions',
+              ],
+              debug: true,
+            }
+          }], 
+          ['@babel/preset-react', {
+            targets: {
+              browsers: [],
+            }
+          }]
+        ],
+        plugins: [],
+      },
+    }],
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({ debug: true }),
+  ],
+  output: {
+    filename: 'app.js',
+    path: path.join(__dirname, 'dist'),
+  }
 }
